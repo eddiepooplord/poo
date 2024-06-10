6fa559c072e3b9b50008876b33ee9752ab0b93d0
eddiepooplord <quuaqg@gmail.com>
2024-06-09 17:55:19 -0700

    Add files via upload

---
+++ final_project/js/script.js
@@ -0,0 +1,29 @@
+function showImage(){
+    var image=document.getElementById('reed');
+    image.src = "images/reed.jpg";
+}
+
+function showScienceImage(){
+    var image=document.getElementById('science');
+    image.src = "images/science.jpg";
+}
+
+function showLitImage(){
+    var image=document.getElementById('lit');
+    image.src = "images/lit.jpg";
+}
+
+function showWebImage(){
+    var image=document.getElementById('web');
+    image.src = "images/web.jpg";
+}
+
+function showbtd6Image(){
+    var image=document.getElementById('btd6');
+    image.src = "images/btd6.jpg";
+}
+
+function showNKImage() {
+    var img = document.getElementById('nobodyknows');
+    img.src = 'images/nobodyknows.jpg';
+}
